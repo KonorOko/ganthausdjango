2cce60bc9e104618d050bfedb803e096141c86a0
KonorOko <125145871+KonorOko@users.noreply.github.com>
2023-12-25 22:28:40 -0600

    database

--- ganthausmanager/settings.py
+++ ganthausmanager/settings.py
@@ -102,8 +102,9 @@ DATABASES = {
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
+        
     )
 }
 
