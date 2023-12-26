4f6e10a74c9fd45557237ef428d181e1d5d9cb7c
KonorOko <125145871+KonorOko@users.noreply.github.com>
2023-12-25 23:36:38 -0600

    alow vercel2.2

--- ganthausmanager/settings.py
+++ ganthausmanager/settings.py
@@ -161,7 +161,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://ganthausreact.vercel.app/"
+    "https://ganthausreact.vercel.app"
 ]
 
 REST_FRAMEWORK = {
