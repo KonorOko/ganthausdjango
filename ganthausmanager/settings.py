d263144ccd372b84691564349c5c8180cb8aa1c3
KonorOko <125145871+KonorOko@users.noreply.github.com>
2023-12-25 21:19:15 -0600

    vercel deploy

--- ganthausmanager/settings.py
+++ ganthausmanager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-g=udm%65=cwb%5r(%i-0=rnz#1w0+tc_x6-a!f*-cl10m2pf4i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
