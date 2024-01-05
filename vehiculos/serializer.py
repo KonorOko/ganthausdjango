7ee1adcebb6ee96feb972bf58bdc5d0ec79fa5af
KonorOko <emilioxkonor@gmail.com>
2024-01-05 08:47:47 -0600

    Servicios, Tenencias Proximos view

--- vehiculos/serializer.py
+++ vehiculos/serializer.py
@@ -15,14 +15,7 @@ class FirstVerificacionesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Verificaciones
         fields = ('vehiculo', 'fecha')
-        
-    def get_queryset(self):
-        if Verificaciones.objects.count() == 0:
-            return []
-        else:
-            queryset = Verificaciones.objects.all().order_by('-fecha')[:3]
-            return queryset
-        
+
 class TenenciasSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tenencias
@@ -32,3 +25,4 @@ class ServiciosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Servicios
         fields = ('id', 'vehiculo', 'fecha')
+        
