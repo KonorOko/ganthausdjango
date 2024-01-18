78334b60eb3012dd57917ebf36eec2f2614e6d41
KonorOko <125145871+KonorOko@users.noreply.github.com>
2024-01-17 20:46:21 -0600

    ventas

--- ventas/models.py
+++ ventas/models.py
@@ -13,6 +13,8 @@ class Ventas(models.Model):
     saldo_pendiente = models.DecimalField(max_digits=10, decimal_places=2)
     
     def save(self, *args, **kwargs):
-        self.saldo_pendiente = (self.total - self.pagado) * self.distribucion
-
+        self.saldo_pendiente = (self.total - self.pagado)
         super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.serie
--- ventas/views.py
+++ ventas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .serializer import VentaSerializer
+from rest_framework import viewsets
+from .models import Ventas
 
 # Create your views here.
+class VentasView(viewsets.ModelViewSet):
+    queryset = Ventas.objects.all()
+    serializer_class = VentaSerializer
