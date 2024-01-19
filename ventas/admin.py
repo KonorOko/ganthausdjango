a87058aacff4648b62cf74f53724c607b57db0a2
KonorOko <emilioxkonor@gmail.com>
2024-01-19 13:42:08 -0600

    remake table 2

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-# from .models import Ventas
+from .models import Ventas
 
-# Register your models here.
-# admin.site.register(Ventas)
+admin.site.register(Ventas)
--- ventas/urls.py
+++ ventas/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from ventas import views
 
 router = routers.DefaultRouter()
-# router.register(r'ventas', views.VentasView, 'ventas')
-# router.register(r'ventaspage', views.VentasPageView, 'ventaspage')
+router.register(r'ventas', views.VentasView, 'ventas')
+router.register(r'ventaspage', views.VentasPageView, 'ventaspage')
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
